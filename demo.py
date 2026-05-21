0c538fbff3cf9ed4f5546a1cc60e59c448504b51
adarsh-456 <adarshdivy@gmail.com>
2026-05-21 10:04:05 +0530

    data ingestion done successfully in pipeline

--- demo.py
+++ demo.py
@@ -1,8 +1,13 @@
-import sys
-from visa.logger import logging
-from visa.exception import USvisaException
+# import sys
+# from visa.logger import logging
+# from visa.exception import USvisaException
 
-try:
-    a = 1 / 0
-except Exception as e:
-    raise USvisaException(e, sys)
+# try:
+#     a = 1 / 0
+# except Exception as e:
+#     raise USvisaException(e, sys)
+from dotenv import load_dotenv
+load_dotenv()
+from visa.pipeline.training_pipeline import TrainingPipeline
+pipeline= TrainingPipeline()
+pipeline.run_pipeline()
--- visa/__init__.py
+++ visa/__init__.py
@@ -2,12 +2,17 @@ import logging
 import os
 from from_root import from_root
 from datetime import datetime
-LOG_FILE = f"{datetime.now().strftime('%m-%d-%Y-%H-%M-%S')}.log"
-log_dir="Logs"
-logs_path=os.path.join(from_root(),log_dir,LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
-logging.basicConfig(filename=logs_path,
-                    format="[%(asctime)s]  %(name)s - %(levelname)s - %(message)s",
-                    level=logging.INFO,
-)                  
 
+LOG_FILE = f"{datetime.now().strftime('%m-%d-%Y-%H-%M-%S')}.log"
+
+log_dir = os.path.join(from_root(), "Logs")
+
+os.makedirs(log_dir, exist_ok=True)
+
+logs_path = os.path.join(log_dir, LOG_FILE)
+
+logging.basicConfig(
+    filename=logs_path,
+    format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
